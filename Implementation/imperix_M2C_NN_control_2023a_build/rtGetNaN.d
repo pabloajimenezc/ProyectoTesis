f8dbb1799d47241a04413cc7b62dffc6f4cac103
matix <matix@DESKTOP-E0717HJ>
2025-10-07 15:28:56 -0300

    07/10/2025 15:23

--- Implementation/imperix_M2C_NN_control_2023a_build/rtGetNaN.d
+++
@@ -1,7 +0,0 @@
-rtGetNaN.o rtGetNaN.o: rtGetNaN.cpp rtwtypes.h rtGetNaN.h rt_nonfinite.h
-
-rtwtypes.h:
-
-rtGetNaN.h:
-
-rt_nonfinite.h:
--- Implementation/imperix_M2C_NN_control_2023a_build/rt_nonfinite.d
+++
@@ -1,5 +0,0 @@
-rt_nonfinite.o rt_nonfinite.o: rt_nonfinite.cpp rtwtypes.h rt_nonfinite.h
-
-rtwtypes.h:
-
-rt_nonfinite.h:
